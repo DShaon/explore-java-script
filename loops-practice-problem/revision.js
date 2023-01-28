039ce848a54bd8a45e297d255ab4a813c63035d3
unknown <toxicshaon@gmail.com>
2023-01-29 00:23:22 +0600

    2nd commit

--- loops-practice-problem/revision.js
+++ loops-practice-problem/revision.js
@@ -21,7 +21,7 @@ var isExpensive = false;
 
 var tableItems = ['bottle', 'pen', 'mouse', 'sunglass'];
 
-// find an index number of an array 
+// find an element of an array 
 var item3 = tableItems[3];
 
 // know length of an array
@@ -121,12 +121,15 @@ if (4 > 5) {
   if (number2 > 20) {
     if (typeof number2 == "number") {
       console.log("number 2 is a number");
-    } else if (typeof number2 == "string") {
+    } 
+    else if (typeof number2 == "string") {
       console.log("wow number 2 is string");
-    } else {
+    } 
+    else {
       console.log("maybe number 2 is boleen");
     }
-  } else {
+  } 
+  else {
     console.log(" number 2 is very small number please try  bigger then 20");
   }
   
@@ -135,8 +138,7 @@ if (4 > 5) {
 
 // while loop is used to repeat code until a condition is met.
 
-// declared a variable for running while loop
-var roastGiven = 0;
+
 
 // while loop should be written in -> while(condition){} 
 // 4 things need to write and run while loop->
@@ -145,10 +147,11 @@ var roastGiven = 0;
 // 3.lopp body
 // 4. change the loop variable, increament or decreament .
 
-
+// declared a variable for running while loop
+var roastGiven = 0;
 while (roastGiven < 7) {
     console.log('roast pabe');
-    // here roastGiven value will increase with 1, and i can use Shorthand to write this type of increasing, example given below - >
+    // here roastGiven value will increase with 1, and  can use Shorthand to write this type of increasing, example given below - >
 
     // roastGiven = roastGiven + 1;
     // roastGiven += 1;
@@ -222,16 +225,6 @@ for (i=1; i<=10; i+=2){
 
 
 // display every element of an array with for loop
-
-
-// bangla system
-var numbers = [45, 87, 89, 56, 32, 51, 25, 52, 68];
-
-for (var i = 0; i < numbers.length; i++) {
-    var number = numbers[i];
-    console.log(number);
-}
-
 
 // good system
 var tableItems = ['bottle', 'pen', 'paper', 'pencil', 'books', 'calculator']
@@ -334,3 +327,45 @@ for (i=0; i<5; i++){
     console.log(i);
 };
 console.log(i);
+
+
+
+
+// *Useful array methods!*!*!**!*!*!*!*!*!*!*!*!**************************************************************************************
+stuff.pop(); // removes the last item in the array (returns what was removed)
+//stuff = ['A', 'B', 'C', 'D', 'E']
+//return: ['F']
+stuff.shift(); // removes the first item in the array (returns what was removed)
+//stuff = ['B', 'C', 'D', 'E', 'F']
+//return: ['A']
+stuff.push("G"); // adds an item to the end of the array (returns the new length of the array)
+//stuff = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+//return: 7
+stuff.unshift("Z"); // adds an item to the beginning of the array (returns the new length of the array)
+//stuff = ['Z', 'A', 'B', 'C', 'D', 'E', 'F']
+//return: 7
+stuff.indexOf("C"); // returns the index number of the item in the array
+//return: 2
+stuff.splice(2); // gets rid of the index item and after it (returns what was removed)
+//stuff = ['A', 'B']
+//return: ['C', 'D', 'E', 'F']
+stuff.splice(2, 2); // second argument determines how many items are removed in total (including index) (returns what was removed)
+//stuff = ['A', 'B', 'E', 'F']
+//return: ['C', 'D']
+stuff.splice(2, 0, "Hello"); // inserts item "hello" into index number 2 and shifts other items ahead (removing 0 items) (returns what was removed)
+//stuff = ['A', 'B', 'Hello', 'C', 'D', 'E', 'F']
+//return: []
+stuff.splice(2, 3, "Hello"); // removes 3 items (including index number 2 and after) and inserts "hello" into index number 2 (returns what was removed)
+//stuff = ['A', 'B', 'Hello', 'F']
+//return: ['C', 'D', 'E']
+stuff.slice(2); // same as splice but doesn't modify the original array (returns what would be removed)
+//stuff = ['A', 'B', 'C', 'D', 'E', 'F']
+//return: ['C', 'D', 'E', 'F']
+stuff.concat(["G", "H", "I"]); // returns the combination of array "stuff" with the array argument
+//return: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
+stuff.toString(); // returns the combination of elements of the array into a string
+//return: "A,B,C,D,E,F"
+stuff.join("-"); // works the same as toString() but the argument determines what is between each element in the string
+//return: "A-B-C-D-E-F"
+stuff.reverse(); // reverses an array's elements (returns same thing)
+//stuff = ['F', 'E', 'D', 'C', 'B', 'A']
