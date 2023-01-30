37ee1d27ad30bc5f579a4901ab52ae5ef4faced9
unknown <toxicshaon@gmail.com>
2023-01-30 16:00:45 +0600

    added function object

---
+++ js-function-object/function.js
@@ -0,0 +1,11 @@
+/*Function declaration rules */
+function startFan() {
+    console.log('Stand up');
+    console.log('Walk towards the switch');
+    console.log('Press the switch');
+
+}
+
+/* Call the function--  */
+startFan(); /* this is the function we need to call to display the output */
+
---
+++ js-function-object/parameter.js
@@ -0,0 +1,26 @@
+function bringSingara(taka) /* here taka is the parameter */
+
+{
+
+    console.log('mama singara dao');
+
+    console.log(taka);
+
+}
+
+bringSingara(200);
+// amra chaile ekadhik parameter dite pari
+
+function add(num1, num2) {
+    console.log('we add', num1, num2)
+}
+
+add(52, 45);
+
+// we can do additon with function by adding multiple parameter
+function sum(a, b, c, d, e, f) {
+    var k = a + b + c + d + e + f
+    console.log(k)
+}
+sum(5, 6, 1);
+
